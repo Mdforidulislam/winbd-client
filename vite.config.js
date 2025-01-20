430461c9fe31a9cd5df37feabbebeb945276ff36
Mdforidulislam <mdforidulislam962@gmail.com>
2025-01-21 01:47:12 +0600

    addednew coehere

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,11 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000, 
+    host: '0.0.0.0', 
+  },
   build: {
-    chunkSizeWarningLimit: 500000, // Example: 500KB
+    chunkSizeWarningLimit: 500, 
   },
 });
